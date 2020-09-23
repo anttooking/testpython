5562f59d894a2eaa23e6314f05dbea248793ef6d
Anthony Mathews <antmmathews@gmail.com>
2020-09-23 12:36:02 +0100

    GTK testing

---
+++ GTK/gtk.py
@@ -0,0 +1,20 @@
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+win = Gtk.Window()
+
+label = Gtk.Label()
+label.set_text("I am a label")
+label.set_size_request(200,200)
+
+combo = Gtk.ComboBox()
+combo.set_size_request(150,60)
+
+win.add(label)
+win.add(combo)
+
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+
+Gtk.main()
