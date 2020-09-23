3e7b733546e99006f0a61980705b75ba46af9819
Anthony Mathews <antmmathews@gmail.com>
2020-09-23 09:31:49 +0100

    Something

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+#fibonaci numbers
+a = 0
+b = 1
+while a < 1000:
+    n = a + b
+    print(n) 
+    a = b
+    b = n
+
+
+def doesKyleSmell(t):
+    fromBlackburn = True 
+    if (fromBlackburn):
+        return True 
+    claimsToBeFromDarwen = True
+
+    if(fromBlackburn == claimsToBeFromDarwen):
+        print('nice try')
