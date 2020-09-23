dfa073dc75b9dbff6fd1a20d6381f4592d46cf79
Anthony Mathews <antmmathews@gmail.com>
2020-09-23 10:24:38 +0100

    added for loop

---
+++ 4/for.py
@@ -0,0 +1,15 @@
+
+
+##basic for loop, works like foreach / iterators
+
+someWords = ['hello world','hello','world']
+for a in someWords:
+    print("The word is", a[0:1])
+
+##range for basics for loop
+for i in range(1,9):
+    print(i, i*i, i*i*i)
+
+
+
+
